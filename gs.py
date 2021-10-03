20a25773a42839bf002d6292aebd3c8deea1ba27
Ranuga-Disansa <79456372+Programmer-RD-AI@users.noreply.github.com>
2021-10-03 21:35:18 +0530

    Add files via upload

---
+++ gs.py
@@ -0,0 +1,7 @@
+from sklearn.model_selection import ParameterGrid
+param_grid = {'param1': ['value1', 'value2', 'value3'], 'paramN' : ['value1', 'value2', 'valueM']}
+
+grid = ParameterGrid(param_grid)
+
+for params in grid:
+    print(params)
